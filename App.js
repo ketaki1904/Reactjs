613ab3fb8029f06aca578c9af18374374fe4cdda
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2024-10-05 23:31:56 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,30 +1,54 @@
-import React, { useEffect, useState } from 'react'
-function App() {
-   const[data,setData]=useState([]);
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(resp => resp.json())
-      .then(json => {
-        console.log(json);
-       setData(json)
-      });
+import  React, { useEffect, useState } from 'react';
+import Navbartop from './Navbartop';
+//import { json } from 'react-router-dom';
+function App(){
+
+  const[Flight,setFLight]=useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:3009/data').then(resp=>resp.json()).then(json=>{
+      console.log(json);
+      setFLight(json);
+    })
   },[])
-  
 
-  return (
 
-      <>
+  return(
+    <div>
+      <Navbartop/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+    <h1 className='desplay-5 text-center'>These is the Data Of Flight Details</h1>
+    <form>
+      <div className='row mt-5'>
+        <div className='col-lg-6 mx-auto'>
+          <input className='form-control'placeholder='Search Your Flight Details'>
+          </input>
+        </div>
+      </div>
+    </form>
+    <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       {
-          data.map((x)=>(
-            <div>
-              <h1>{x.id}</h1>
+        Flight.map((x)=>(
+          <div key={x.id}>
+            <div className='card'style={{width:"18rem"}}>
+            <img src={x.image} alt='flight-data'/>
+            <div className='card-body'></div>
+            <div className='card-title h3'>{x.title}</div>
+            <div className='card-title h3'>Airline:{x.airline}</div>
+            <div className='card-title h3'>captain{x.captain}</div>
+
             </div>
-
-          ))
-        }
-        </>
-
+          </div>
+        ))
+      }
+    </div>
   )
 }
-
 export default App;
