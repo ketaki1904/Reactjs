f1abf48412254b7f10c4aeae4db9b1629bd52602
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2024-10-06 20:16:48 +0530

    Add files via upload

---
+++ Navbartop.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Navbartop() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg bg-none fixed-top">
+                <div className="container">
+                    <div className="navbar brand">TCS</div>
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-items">
+                            <Link className="nav-link" to={'/'}>App component</Link>
+                        </li>
+
+                        <li className="nav-items">
+                        <Link className="nav-link" to={'/Mountain'}>Mountain component</Link>
+
+
+                        </li>
+
+                        <li className="nav-items">
+                        <Link className="nav-link" to={'/Fire'}>Fire component</Link>
+
+                        </li>
+                    </ul>
+                </div>
+
+            </nav>
+        </div>
+
+
+    )
+}
+export default Navbartop
