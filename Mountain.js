c788caec0f335ba4e6b62f8569232860718024c9
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2024-10-21 16:59:38 +0530

    Add files via upload

---
+++ Mountain.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbartop from "./Navbartop";
+function Mountain(){
+    return(
+<div>
+<Navbartop/>
+<br>
+</br>
+<h1>This is Mountain</h1>
+
+</div>
+    )
+}
+export default Mountain
