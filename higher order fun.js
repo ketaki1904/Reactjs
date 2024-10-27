a00e5ac1317edbff7ee58eb7988e1107ec787529
Ketaki Chandak <159540699+ketaki1904@users.noreply.github.com>
2024-10-27 21:26:38 +0530

    higher order fun.js

--- higher order fun.js
+++ higher order fun.js
@@ -6,14 +6,50 @@
 }
 
 //mountain .js
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Mountain=({name,email,contact})=>{
+    const[count,updatecount]=useState(0);
+    const[data,setData]= useState([]);
+    const[form, setForm]=useState('');
+    const getdata=()=>{
+        alert(`${form}`);
+    }
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(resp=>resp.json()).then(json=>{
+            console.log(json);
+            setData(json);
+        },[])
+      
+    })
     return(
         <>
         <h1 className="mt-5">These is the Higher Order function</h1>
-        <h1>Hi My Name Is{name} and my email is {email} and my contact number is {contact} </h1>
+        <h1> Hi My Name is {name} and my gamil{email}and my contact no is{contact}</h1>
+        <button onClick={()=>alert("this is onclick event")}>click</button>
+        <h1>{count}</h1>
+        < button onClick={()=>{updatecount(count+1)}}>updatecount</button>
+        {
+            data.map((x)=>(
+                <div key={x.id}>
+                    <h1>{x.title}</h1>
+                </div>
+            ))
+        
+        }
+        <h1> these is how we will execute the form validation</h1>
+        <form onSubmit={()=>{getdata()}}>
+            <input value={form} onChange={(e)=>{setForm(e.target.value)}} className="form-control" placeholder="Enter your name"/>
+            <button className="btn btn-primary" type="submit">submit</button>
+        </form>
         </>
     )
 }
 export default Mountain;
+
+
+
+
+        
+
